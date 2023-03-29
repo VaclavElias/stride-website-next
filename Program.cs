07cc228c3142fb761f5a80c402c6f132b6adc85d
Vaclav Elias <vaclavelias@gmail.com>
2023-03-29 22:53:44 +0100

    ASP.NET Core Minimal API added, if needed to server in ASP.NET Core environemnt

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = "_site"
+});
+var app = builder.Build();
+
+//app.MapGet("/stride", () => "Hello Stride!");
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
