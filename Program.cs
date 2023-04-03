eec9527aa1bb6e94d623fad2b3e9257a9e1470a9
Vaclav Elias <vaclavelias@gmail.com>
2023-04-03 23:56:24 +0100

    feat: Compression and caching added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     WebRootPath = "_site" // Set the folder where the static files are located (e.g., Eleventy output folder)
 });
 
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
+
 var app = builder.Build();
+
+app.UseResponseCompression();
 
 // Just a test
 app.MapGet("/stride", () => "Hello Stride!");
@@ -12,10 +19,21 @@ app.MapGet("/stride", () => "Hello Stride!");
 // Configure the application to redirect 404 (Not Found) errors to a custom 404.html page
 app.UseStatusCodePagesWithReExecute("/404.html");
 
+app.UseHttpsRedirection();
+
 // Set up the application to serve the index.html file when the root path is requested
 app.UseDefaultFiles();
 
+var cacheMaxAgeOneYear = (60 * 60 * 24 * 365).ToString();
+
 // Enable serving static files from the specified WebRootPath
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append(
+             "Cache-Control", $"public, max-age={cacheMaxAgeOneYear}");
+    }
+});
 
 app.Run();
