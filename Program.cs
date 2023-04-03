18bf95455e327e6c6fb504f2a266bfb431ed92bf
Vaclav Elias <vaclavelias@gmail.com>
2023-04-03 23:24:22 +0100

    feat: Updating ASP.NET Core part to change wwwroot to _site

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    WebRootPath = "_site"
+});
 
 var app = builder.Build();
 
-//app.MapGet("/stride", () => "Hello Stride!");
+app.MapGet("/stride", () => "Hello Stride!");
 
 app.UseDefaultFiles();
+
 app.UseStaticFiles();
 
 app.Run();
